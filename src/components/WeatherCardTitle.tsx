56d56be896d346107c55e75c5ebf974917273c3f
jimmy89Li <liviu.c.simion@gmail.com>
2025-05-10 15:09:48 +0200

    Add className prop to WeatherCardTitle for customizable styling

--- src/components/WeatherCardTitle.tsx
+++ src/components/WeatherCardTitle.tsx
@@ -1,12 +1,14 @@
 export const WeatherCardTitle = ({
   title,
   subtitle,
+  className,
 }: {
   title?: string;
   subtitle?: string;
+  className?: string;
 }) => {
   return (
-    <p className='text-xl'>
+    <p className={'text-xl' + (className ? ` ${className}` : '')}>
       {title && <strong>{title}</strong>}
       {subtitle && <span>{subtitle}</span>}
     </p>
