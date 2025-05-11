6aadf7631815532cd5fa7d08e871bc9aa2e6266e
jimmy89Li <liviu.c.simion@gmail.com>
2025-05-11 18:31:16 +0200

    Add text color to Button component for improved visibility

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
 
 export const Button = ({ className, children, ...rest }: ButtonProps) => {
-  let buttonClass = `h-10 w-20 p-1 rounded-sm bg-blue-400
+  let buttonClass = `h-10 w-20 p-1 rounded-sm text-black bg-blue-400
     border-3 border-blue-400 hover:border-blue-500 active:border-blue-600`;
   buttonClass += className ? ` ${className}` : '';
 
